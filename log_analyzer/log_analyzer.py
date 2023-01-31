9d654c70534455c90988b60b0f5d74b495289cc4
bonifacyi <58585123+bonifacyi@users.noreply.github.com>
2023-01-31 15:40:12 +0300

    optimise and fix
    optimise log_data_generator; debug

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -12,11 +12,13 @@ import gzip
 import json
 from datetime import datetime
 from collections import defaultdict
+from string import Template
 
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "../data/reports",
     "LOG_DIR": "../data/log",
+    "TEMPLATE": "../static/report.html",
     "LOG_PATTERN": "nginx-access-ui\\.log-(\\d*)(\\.gz)?"
 }
 
@@ -31,21 +33,21 @@ def get_last_log_file(log_dir, log_pattern):
     :return: log_date: datetime object
     :return: log_compress: str or None
     """
-    log_dir_abs = os.path.abspath(log_dir)
     log_file_path = None
     log_date = 0
     log_compress = None
 
-    for filename in os.listdir(log_dir_abs):
+    for filename in os.listdir(log_dir):
         m = re.match(log_pattern, filename)
         if m is not None:
             date, compress = m.groups()
+            date = int(date)
             if (date > log_date) or (date == log_date and compress is None):
                 log_file_path, log_date, log_compress = filename, date, compress
 
     if log_file_path:
-        log_file_path = os.path.join(log_dir_abs, log_file_path)
-        log_date = datetime.strptime(log_date, '%Y%m%d')
+        log_file_path = os.path.join(log_dir, log_file_path)
+        log_date = datetime.strptime(str(log_date), '%Y%m%d')
 
     return log_file_path, log_date, log_compress
 
@@ -58,15 +60,18 @@ def log_data_generator(open_file, log_compress=None):
     :return: request_url: str
     :return: request_time: float
     """
-    url_pattern = '"[A-Z]+\\s([^\\s]+)'
-    time_pattern = '([\\.\\d]+)$'
+    pattern = '"[A-Z]+\\s([^\\s]+).+?([\\.\\d]+)$'
+    compile_pattern = re.compile(pattern)
 
     for line in open_file:
         line = line.decode('utf-8') if log_compress else line
         line = line.strip()
-        request_url = re.search(url_pattern, line).groups()[0]
-        request_time = re.search(time_pattern, line).groups()[0]
-        request_time = float(request_time)
+        request_url, request_time = None, None
+
+        res = compile_pattern.search(line)
+        if res is not None:
+            request_url, request_time = res.groups()
+            request_time = float(request_time)
 
         yield request_url, request_time
 
@@ -78,7 +83,7 @@ def calculate_json_table(aggregated_data, total_request_time, total_count, repor
     :param total_request_time: float
     :param total_count: int
     :param report_size: int
-    :return:
+    :return: json_table: str(json)
     """
     table = list()
     for url, time_list in aggregated_data.items():
@@ -86,20 +91,39 @@ def calculate_json_table(aggregated_data, total_request_time, total_count, repor
 
         url_data['url'] = url
         url_data['count'] = len(time_list)
-        url_data['time_max'] = max(time_list)
-        url_data['time_sum'] = sum(time_list)
-        url_data['time_avg'] = url_data['time_sum'] / url_data['count']
-        url_data['time_perc'] = 100 * url_data['time_sum'] / total_request_time
-        url_data['count_perc'] = 100 * url_data['count'] / total_count
+        url_data['time_max'] = round(max(time_list), 3)
+        url_data['time_sum'] = round(sum(time_list), 3)
+        url_data['time_avg'] = round(url_data['time_sum'] / url_data['count'], 3)
+        url_data['time_perc'] = round(100 * url_data['time_sum'] / total_request_time, 3)
+        url_data['count_perc'] = round(100 * url_data['count'] / total_count, 3)
         middle = url_data['count'] // 2
         if url_data['count'] % 2 != 0:
-            url_data['time_med'] = sorted(time_list)[middle]
+            url_data['time_med'] = round(sorted(time_list)[middle], 3)
         else:
-            url_data['time_med'] = sum(sorted(time_list)[middle - 1:middle + 1]) / 2
+            url_data['time_med'] = round(sum(sorted(time_list)[middle - 1:middle + 1]) / 2, 3)
 
         table.append(url_data)
 
     sorted_table = sorted(table, key=lambda d: d['time_sum'], reverse=True)[:report_size]
+    json_table = json.dumps(sorted_table)
+    return json_table
+
+
+def rendering_report(table, template_path, report_path):
+    """
+
+    :param table: str(json)
+    :param template_path: str(full path)
+    :param report_path: str(full path)
+    :return: None
+    """
+    with open(template_path, 'r') as tmpl:
+        template_html = Template(tmpl.read())
+
+    report_html = template_html.safe_substitute(table_json=table)
+
+    with open(report_path, 'w') as report:
+        report.write(report_html)
 
 
 def aggregate_log_data(data):
@@ -107,15 +131,21 @@ def aggregate_log_data(data):
 
     :param data: generator object
         tuple: request_url, request_time
-    :return:
+    :return: aggregated_data: dict
+    :return: total_request_time: float
+    :return: total_count: int
     """
     total_count = 0
     total_request_time = float()
     aggregated_data = defaultdict(list)
     for request_url, request_time in data:
+        if (request_url is None) or (request_time is None):
+            continue
         aggregated_data[request_url].append(request_time)
         total_request_time += request_time
         total_count += 1
+
+    return aggregated_data, total_request_time, total_count
 
 
 def get_log_report(conf):
@@ -124,10 +154,28 @@ def get_log_report(conf):
     :param conf: dict
     :return:
     """
-    log_file_path, log_date, log_compress = get_last_log_file(conf['LOG_DIR'], conf['LOG_PATTERN'])
+    log_dir = os.path.abspath(conf['LOG_DIR'])
+    template_path = os.path.abspath(conf['TEMPLATE'])
 
-    open_file = gzip.open(log_file_path, 'rb') if log_compress else open(log_file_path, 'r')
-    data = log_data_generator(open_file, log_compress)
+    log_file_path, date, compress = get_last_log_file(log_dir, conf['LOG_PATTERN'])
+
+    open_file = gzip.open(log_file_path, 'rb') if compress else open(log_file_path, 'r')
+    log_data = log_data_generator(open_file, compress)
+    aggregated_data, total_request_time, total_count = aggregate_log_data(log_data)
+    open_file.close()
+
+    json_table = calculate_json_table(
+        aggregated_data,
+        total_request_time,
+        total_count,
+        conf['REPORT_SIZE']
+    )
+
+    report_path = os.path.join(
+        os.path.abspath(conf['REPORT_DIR']),
+        f'report-{date.strftime("%Y")}.{date.strftime("%m")}.{date.strftime("%d")}.html'
+    )
+    rendering_report(json_table, template_path, report_path)
 
 
 def main():
